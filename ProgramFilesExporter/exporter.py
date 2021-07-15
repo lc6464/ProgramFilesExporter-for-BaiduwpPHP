3219004564aebb332abead1d91aca3849058040c
lc6464 <64722907+lc6464@users.noreply.github.com>
2021-07-15 09:43:39 +0800

    自 Baiduwp-PHP 项目的 ProgramFilesExporter 分支更新而来，原仓库分支将被删除

---
+++ ProgramFilesExporter/exporter.py
@@ -0,0 +1,47 @@
+from json import load as loadJSON
+from json import dumps as dumpsJSON
+import os, re, shutil, sys
+from os import path
+
+
+
+
+try: # 尝试读取排除规则列表
+	with open('./exclude-list.json', encoding="utf-8") as f:
+		excludeList = loadJSON(f)
+except: # 若读取失败
+	print('Read-ExcludeList-Error')
+	sys.exit(0)
+
+
+
+files = []
+
+try: # 尝试获取文件列表
+	for name in os.listdir("../"):
+		rs = []
+		for i in excludeList: # 获取排除列表
+			rs.append(re.search(i, name) == None) # 判断是否符合排除规则
+		if False not in rs: # 获取文件列表
+			files.append('../' + name)
+	print(dumpsJSON(files))
+except: # 若获取失败
+	print('Get-FileList-Error')
+	sys.exit(0)
+
+
+try: # 尝试复制文件
+	if not path.exists('../ProgramFiles/'): # 若文件夹不存在则创建
+		os.mkdir('../ProgramFiles/')
+	for root, dirs, subFiles in os.walk('../ProgramFiles/', topdown=False): # 清空文件夹
+		for name in subFiles:
+			os.remove(path.join(root, name))
+		for name in dirs:
+			os.rmdir(path.join(root, name))
+	for name in files: # 复制文件和文件夹
+		shutil.copytree(name, '../ProgramFiles/' + name[3:]) if path.isdir(name) else shutil.copy(name, '../ProgramFiles/')
+	print('导出成功！')
+except:
+	print('Copy-Files-Error')
+	print('You can copy these files by youself:')
+	print(dumpsJSON(files))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ProgramFilesExporter-for-BaiduwpPHP
+## Baiduwp-PHP 项目程序文件导出工具
+
 为 Baiduwp-PHP 项目制作的程序文件导出工具，由 Python 编写。
+方便上传 assets，也为未来的自动更新做准备。

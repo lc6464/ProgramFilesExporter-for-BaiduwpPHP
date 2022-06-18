bb13272b734485c3f2e8d6b7a3a3222c9acb34ce
lc6464 <64722907+lc6464@users.noreply.github.com>
2022-06-18 23:01:57 +0800

    新增压缩功能

--- ProgramFilesExporter/exporter.py
+++ ProgramFilesExporter/exporter.py
@@ -1,6 +1,6 @@
 from json import load as loadJSON
 from json import dumps as dumpsJSON
-import os, re, shutil, sys
+import os, re, shutil, sys, zipfile
 from os import path
 
 
@@ -10,7 +10,7 @@ try: # 尝试读取排除规则列表
 	with open('./exclude-list.json', encoding="utf-8") as f:
 		excludeList = loadJSON(f)
 except: # 若读取失败
-	print('Read-ExcludeList-Error')
+	print('读取排除规则列表失败！')
 	sys.exit(0)
 
 
@@ -26,7 +26,7 @@ try: # 尝试获取文件列表
 			files.append('../' + name)
 	print(dumpsJSON(files))
 except: # 若获取失败
-	print('Get-FileList-Error')
+	print('获取文件列表失败！')
 	sys.exit(0)
 
 
@@ -46,7 +46,23 @@ try: # 尝试复制文件
 	for name in files: # 复制文件和文件夹
 		shutil.copytree(name, '../ProgramFiles/' + name[3:]) if path.isdir(name) else shutil.copy(name, '../ProgramFiles/')
 	print('导出成功！')
+	print('-' * 20)
+	print('正在压缩中……')
+	try: # 压缩
+		walkResult = tuple(os.walk('../ProgramFiles/'))
+		f = zipfile.ZipFile('../ProgramFiles/ProgramFiles.zip', 'w', zipfile.ZIP_DEFLATED)
+		try:
+			for root, dirs, subFiles in walkResult: # 压缩文件夹
+				for name in subFiles:
+					f.write(path.join(root, name), '%s/%s' % (root.replace('../ProgramFiles/', ''), name), compresslevel=9)
+			print('压缩成功！')
+		except:
+			print('压缩失败！')
+		finally:
+			f.close()
+	except:
+		print('压缩失败！')
 except:
-	print('Copy-Files-Error')
-	print('You can copy these files by youself:')
+	print('复制文件失败！')
+	print('您可以自行复制这些文件：')
 	print(dumpsJSON(files))
